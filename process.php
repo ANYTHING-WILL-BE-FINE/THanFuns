e3aa5003971e1d1917a4467e07ecec770a3f6ac7
arjree321 <arjaree321@gmail.com>
2022-05-12 02:06:27 +0700

    eyyy

--- process.php
+++ process.php
@@ -1,1 +1,14 @@
-<?php include("connect_db.php");?>
+<?php include("src/php/connect_db.php");?>
+<?php
+    // print_r($_POST)
+    $file_id = $_POST['file_id'];
+    // $product_name = $_POST[product_name];
+    // $description = $_POST[description];
+    // $tags_label = $_POST[tags_label];
+    // $label = $_POST[label];
+    // $mature_mode = $_POST[mature_mode];
+    // $sale_mode = $_POST[sale_mode];
+
+    mysqli_query($conn, "INSERT INTO pth_file (file_path)
+                                VALUE ('$file_id')");
+?>
