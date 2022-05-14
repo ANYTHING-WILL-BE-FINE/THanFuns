8e992a0e68ab55c2e3e9927ffef767224acf9bb7
arjree321 <arjaree321@gmail.com>
2022-05-14 12:02:57 +0700

    Merge branch 'main' of https://github.com/ANYTHING-WILL-BE-FINE/THanFuns

--- process.php
+++ process.php
@@ -21,8 +21,7 @@ if($action == "insertCommission") {
   $extend = $_POST['extend'];
   $status = $_POST['status'];
   $sql = "INSERT INTO mkt_commission(user_id,creator_id,request_price,job_color,job_scale,job_description,job_mature,job_private,commission_id,first_pay,job_co_right_mode,datetime_limit,extend_time_status,commission_status)
-   VALUES ($iduser,$idcreator,$price,$color,$scale,"$description",$mature,$publiceArt,$idcommission,$firstpay,$mode,"$datetime",$extend,$status)";
-  $mysql = mysqli_query($conn, $sql);
+   VALUES ($iduser,$idcreator,$price,$color,$scale,'$description',$mature,$publiceArt,$idcommission,$firstpay,$mode,'$datetime')";
   if ($mysql === TRUE) {
     echo "New record has been added successfully !";
  } else {
